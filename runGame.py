6806bb6a3a5c517b0bde567f2eada862b66d382f
mattlourenco27 <matt.lourenco27@gmail.com>
2020-03-10 22:57:08 -0400

    Initial contruction of tile class file and main file

---
+++ runGame.py
@@ -0,0 +1,7 @@
+# Created on 10 Mar 2020
+# Created by: Matthew Lourenco
+# This file plays the mines game
+
+import tile
+
+pass
---
+++ tile.py
@@ -0,0 +1,6 @@
+# Created on 10 Mar 2020
+# Created by: Matthew Lourenco
+# This file is a class for a tile in the mines game
+
+class Tile:
+    pass
