cc6f3ad1d3395c6da163722bb89f1a4cd379db0d
mattlourenco27 <matt.lourenco27@gmail.com>
2020-05-08 19:38:49 -0400

    Initial commit of run.py and gui.py Screen and icon appear

--- gui.py
+++ gui.py
@@ -2,4 +2,58 @@
 # Created by: Matthew Lourenco
 # This file defines class and methods used to control the mines GUI
 
-pass
+import pygame
+import game
+import solver
+
+class Gui:
+    CANVAS_SIZE = 800
+    COMMANDS_BAR_SIZE = 100
+
+    # colours
+    GRAY_BLUE = (186, 216, 219)
+
+    def __init__(self):
+        # game clock
+        self.clock: pygame.time.Clock
+
+        # screen
+        self.screen: pygame.display.Surface
+
+        # Initialize the game
+        pygame.init()
+
+        pygame.display.set_caption("'Mines' made by mattlourenco27 on Github")
+        # Icon made by Creaticca Creative Agency from www.flaticon.com
+        icon = pygame.image.load("./assets/sprites/weapons.png")
+        pygame.display.set_icon(icon)
+
+    # Start the gui
+    def start(self):
+        # Create the game clock
+        self.clock = pygame.time.Clock()
+
+        # Create the screen
+        self.screen = pygame.display.set_mode((Gui.CANVAS_SIZE, Gui.CANVAS_SIZE + Gui.COMMANDS_BAR_SIZE))
+
+        # start the game loop
+        self._game_loop()
+
+    def _draw_base_canvas(self):
+        self.screen.fill(Gui.GRAY_BLUE)
+
+    def _draw_screen(self):
+        self._draw_base_canvas()
+        pygame.display.update()
+
+    def _game_loop(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    # Exit the game
+                    pygame.quit()
+                    quit()
+
+            self._draw_screen()
+            self.clock.tick(60)
--- run.py
+++ run.py
@@ -2,4 +2,10 @@
 # Created by: Matthew Lourenco
 # This file plays the mines game in a GUI
 
-pass
+import gui
+
+if __name__ == "__main__":
+    print("Welcome to Mines! Created by mattlourenco27 on github")
+    print("Starting . . .")
+    screen = gui.Gui()
+    screen.start()
